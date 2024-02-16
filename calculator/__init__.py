94ec7f57dd8065c79b273e2accaf663d470dd871
haren0612 <haren0612@gmail.com>
2024-02-16 02:23:08 -0500

    fixes

---
+++ calculator/__init__.py
@@ -0,0 +1,23 @@
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculator:
+  @staticmethod
+  def add(a, b):
+    calculation = Calculation(a, b, add)
+    return calculation.perform()
+  
+  @staticmethod
+  def subtract(a, b):
+    calculation  = Calculation(a, b, subtract)
+    return calculation.perform()
+  
+  @staticmethod
+  def multiply(a, b):
+    calculation = Calculation(a, b, multiply)
+    return calculation.perform()
+  
+  @staticmethod
+  def divide(a, b):
+    calculation = Calculation(a, b, divide)
+    return calculation.perform()
